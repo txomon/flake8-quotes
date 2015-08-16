3e9321cdc5278cfdfdcd6b13caea2e33e24e7745
Javier Domingo Cansino <javierdo1@gmail.com>
2015-08-16 23:00:07 +0200

    Just discovered I have been duplicating work :( https://github.com/zheller/flake8-quotes which is already registered in pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
+
 def get_version(fname='flake8_quotes.py'):
     with open(fname) as f:
         for line in f:
             if line.startswith('__version__'):
                 return eval(line.split('=')[-1])
 
+
 setup(
-    name='flake8-quotes',
+    name='flake8-quotes2',
     description='flake8 plugin for string quotes checking',
     license='MIT',
     long_description=open('README.rst').read(),
@@ -21,9 +23,19 @@ setup(
         'flake',
     ],
     classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
     ],
     entry_points={
-        'flake8.extension': ['flake8_quotes = flake8_quotes:QuotesChecker'],
+        'flake8.extension': ['Q1 = flake8_quotes:QuotesChecker'],
     },
 )
